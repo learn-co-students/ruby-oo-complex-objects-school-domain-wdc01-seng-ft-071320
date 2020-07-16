f3ca679d2469c630c520cb636a212a783adaccd5
acparkgh <acpark123@gmail.com>
2020-07-15 23:24:25 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,9 +12,10 @@ class School
   end
 
   def add_student(student_name, student_grade)
-    unless @roster.include?(student_grade)
-      @roster[student_grade] = []
-    end
+    # unless @roster.include?(student_grade)
+    #   @roster[student_grade] = []
+      @roster[student_grade] ||= []
+    # end
       @roster[student_grade] << student_name
   end
 
