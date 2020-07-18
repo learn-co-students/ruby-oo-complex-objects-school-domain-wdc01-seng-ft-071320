70e884df6f4ca537b20b641248543c985338fdd6
JinaStrickland <Jina30@gmail.com>
2020-07-18 14:26:28 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+class School
+    attr_reader :name
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade) 
+        roster[grade] = []
+        roster[grade] << student
+        # roster << self
+        # roster 
+        # all_student = roster.merge(new)
+        # return all_student
+    end
+
+
+
+end
+
+# school.add_student("AC Slater", 9)
+# school.add_student("Kelly Kapowski", 10)
+# school.add_student("Screech", 11)
+# school.roster
+
